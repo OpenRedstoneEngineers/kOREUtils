58f4378e362df28ae73f6cc72412ef6894017b2a
Pauli Kauro <3965357+paulikauro@users.noreply.github.com>
2021-07-05 19:37:59 +0300

    Update versions and get rid of workflow

--- versions/build.gradle.kts
+++ versions/build.gradle.kts
@@ -3,11 +3,39 @@ plugins {
     `maven-publish`
 }
 
-version = "latest"
+group = "org.openredstone"
+version = "master-SNAPSHOT"
 
 catalog {
     versionCatalog {
-        from(files("versions.toml"))
+        version("ktor", "1.6.1")
+        version("serialization", "1.2.1")
+        version("coroutines", "1.5.0")
+        alias("bungee-api").to("net.md-5:bungeecord-api:1.16-R0.5-SNAPSHOT")
+        alias("spigot-api").to("org.spigotmc:spigot-api:1.16.2-R0.1-SNAPSHOT")
+
+        fun bunch(group: String, prefix: String, versionRef: String, vararg modules: String) {
+            modules.forEach { module ->
+                val artifact = "$prefix-$module"
+                alias(artifact).to(group, artifact).versionRef(versionRef)
+            }
+        }
+
+        bunch(
+            group = "org.jetbrains.kotlinx",
+            prefix = "kotlinx-serialization",
+            versionRef = "serialization",
+            "core", "cbor"
+        )
+
+        alias("kotlinx-coroutines-core").to("org.jetbrains.kotlinx", "kotlinx-coroutines-core").versionRef("coroutines")
+
+        bunch(
+            group = "io.ktor",
+            prefix = "ktor",
+            versionRef = "ktor",
+            "server-core", "server-netty", "serialization", "auth", "client-apache", "html-builder", "locations"
+        )
     }
 }
 
@@ -15,16 +43,6 @@ publishing {
     publications {
         create<MavenPublication>("versions") {
             from(components["versionCatalog"])
-        }
-    }
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/OpenRedstoneEngineers/kOREUtils")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
         }
     }
 }
